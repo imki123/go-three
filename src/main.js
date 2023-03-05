4efb499344a91c6ed09e34b69031ff94700f3fab
hoodie <popping2606@gmail.com>
2023-03-06 02:58:46 +0900

    feat: Add cube

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { Scene } from 'three'
-
-const scene = new Scene()
-console.log(scene)
-const controls = new OrbitControls(camera, renderer.domElement)
-const a = 10
-a = 20
-
-console.log(controls)
